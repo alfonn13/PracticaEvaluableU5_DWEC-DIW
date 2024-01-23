cfb96513bae839f790cdea9a6dcd9a851a7cccc3
alfon01 <101067894+alfon01@users.noreply.github.com>
2024-01-23 17:36:45 +0100

    Update

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,12 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
     var menuHamburguesa = document.getElementById("menu_hamburguesa");
 
     menuHamburguesa.addEventListener("change", function() {
-        // Verificar si el botón de hamburguesa está activo
         if (menuHamburguesa.checked) {
-            // Bloquear el desplazamiento
             document.body.style.overflow = "hidden";
         } else {
-            // Habilitar el desplazamiento
             document.body.style.overflow = "visible";
         }
     });
@@ -26,13 +23,12 @@ function toggleMenu() {
     var hamburgerIcon = document.getElementById('hamburger-icon');
     var crossIcon = document.getElementById('cross-icon');
 
-    // Verificar el estado actual y cambiar la visibilidad en consecuencia
     if (hamburgerIcon.style.display === 'none') {
-        // Si el ícono de hamburguesa está oculto, mostrarlo y ocultar la cruz
+
         hamburgerIcon.style.display = '';
         crossIcon.style.display = 'none';
     } else {
-        // Si el ícono de hamburguesa está visible, ocultarlo y mostrar la cruz
+        
         hamburgerIcon.style.display = 'none';
         crossIcon.style.display = '';
     }
